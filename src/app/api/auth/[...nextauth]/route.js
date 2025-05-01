facf5867aa8038de2c19e076bb1016690dad01d6
Hamza-Nayab <Hamza.nayab48@gmail.com>
2025-05-02 03:23:22 +0500

    worked?

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,18 @@
+import client from "@/libs/mongoClient";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-
-export const authOptions={
-
-    providers:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ]
+export const authOptions = {
+  adapter: MongoDBAdapter(client),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 };
 
-const handler = NextAuth({});
+const handler = NextAuth(authOptions); 
 
 export { handler as GET, handler as POST };
--- src/components/buttons/logingoogle.js
+++ src/components/buttons/logingoogle.js
@@ -1,12 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+"use client";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { signIn } from "next-auth/react";
 
 export default function LoginGoogle() {
-    return (
-        <button className="bg-white shadow text-center w-full py-4 flex gap-4 justify-center">
-        <FontAwesomeIcon icon={faGoogle} className="w-6 " />
-        <span>Sign in with Google</span>
-      </button>
-    );
+  return (
+    <button
+      onClick={() => signIn("google")}
+      className="bg-white shadow text-center w-full py-4 flex gap-4 justify-center"
+    >
+      <FontAwesomeIcon icon={faGoogle} className="w-6" />
+      <span>Sign in with Google</span>
+    </button>
+  );
 }
